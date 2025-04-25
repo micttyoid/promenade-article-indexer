59f6995255bf7b7a99912286f966aea5a359d645
Luke Yoo <w.lukeyoo@gmail.com>
2025-04-25 12:50:06 -0700

    Add traverse/general.py

--- article_indexer/traverse/condies.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-from typing import Callable
-
-
-def if_path_then(predicate: Callable[[Path], bool]) -> Callable[[Path], bool]:
-    """
-    Returns a function that conditionally applies `fn1` or `fn2` based on `predicate`
-    """
-    return lambda fn1, fn2: lambda path: fn1(path) if predicate(path) else fn2(path)
--- article_indexer/traverse/general.py
+++ article_indexer/traverse/general.py
@@ -1,6 +1,5 @@
-from typing import Callable
 from pathlib import Path
-
+from typing import Callable
 from article_indexer.utils.reduce import shortcircuit_reduce
 
 
@@ -25,3 +24,10 @@ def process_directory(
         return True
 
     return shortcircuit_reduce(reducer, root.iterdir(), True)
+
+
+def if_path_then(predicate: Callable[[Path], bool]) -> Callable[[Path], bool]:
+    """
+    Returns a function that conditionally applies `fn1` or `fn2` based on `predicate`
+    """
+    return lambda fn1, fn2: lambda path: fn1(path) if predicate(path) else fn2(path)
